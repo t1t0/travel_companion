42fca6b673007408e3908bf33bda4fafc703f446
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2022-02-02 10:41:23 -0400

    Initial Layout for the Map component

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import { Rating } from '@material-ui/lab';
+
+import useStyles from './styles';
+
+const Map = () =>{
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0 };
+
+    return(
+        <div className={ classes.mapContainer }>
+            <GoogleMapReact bootstrapURLKeys={{ key: 'AIzaSyAnAvSGvP3NC4oEp53s7EHJ3MCS20U7RKs' }}
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={14}
+            margin={[50, 50, 50, 50]}
+            options={''}
+            onChange={''}
+            onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
+    );
+}
+
+export default Map;
